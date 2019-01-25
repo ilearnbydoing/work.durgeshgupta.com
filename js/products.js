13f36c6540e1840b210e2e16efe119dff0890547
ilearnbydoing <dgweb123@gmail.com>
2019-01-25 14:16:33 +0530

    First commit

---
+++ js/products.js
@@ -0,0 +1,127 @@
+var $products,
+
+	$current_product = 'smtpconnect';
+
+
+
+// List all the products here
+
+
+
+$products = {
+		blogwebanaya : {
+
+		name     : 'Blog.Webanaya.com',
+
+		tag      : 'PHP',
+
+		img      : '../images/blog-webanaya.jpg',
+
+		url      : 'https://blog.webanaya.com',
+
+		purchase : '#',
+
+	},
+		emailwebanaya : {
+
+		name     : 'Email.Webanaya.com',
+
+		tag      : 'PHP',
+
+		img      : '../images/email-webanaya.jpg',
+
+		url      : 'https://email.webanaya.com',
+
+		purchase : '#',
+
+	},
+
+		newrampgreen : {
+
+		name     : 'New Ram Green Technologies',
+
+		tag      : 'WP',
+
+		img      : 'images/newrampgreen.jpg',
+
+		url      : 'http://newrampgreen.com/',
+
+		purchase : '#',
+
+	},
+
+		niibindia : {
+
+		name     : 'Niib India',
+
+		tag      : 'WP',
+
+		img      : 'images/niibindia.jpg',
+
+		url      : 'http://niibindia.org/',
+
+		purchase : '#',
+
+	},
+		sanskara : {
+
+		name     : 'Sanskara Trust',
+
+		tag      : 'WP',
+
+		img      : 'images/sanskara.jpg',
+
+		url      : 'http://sanskara.org',
+
+		purchase : '#',
+
+	},
+		w3update : {
+
+		name     : 'W3update',
+
+		tag      : 'WP',
+
+		img      : 'images/w3update.jpg',
+
+		url      : 'https://www.w3update.com/',
+
+		purchase : '#',
+
+	},
+
+	welgrowindia : {
+
+		name     : 'Welgrowindia.com',
+
+		tag      : 'WP',
+
+		img      : 'images/welgrowindia.jpg',
+
+		url      : 'http://welgrowindia.com/',
+
+		purchase : '#',
+
+	},
+
+	ti2 : {
+
+		name     : 'Ti2.com.au',
+
+		tag      : 'WP',
+
+		img      : 'images/ti2.jpg',
+
+		url      : 'https://ti2.com.au/',
+
+		purchase : '#',
+
+	},
+
+
+
+
+
+
+
+};
